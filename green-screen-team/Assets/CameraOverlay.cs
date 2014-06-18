6cbe8d5c3713088e7124832e3f9b3ea7b1c33bfe
Nick Kornek <nkornek@gmail.com>
2014-06-18 16:56:42 -0400

    coding for camera UI

---
+++ green-screen-team/Assets/CameraOverlay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOverlay : MonoBehaviour {
+	public bool isRec ;
+	// Use this for initialization
+	void Start () {
+		var isRec = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRec = false)
+		{
+			GameObject.Find("Lens").GetComponent<SpriteRenderer>().enabled = false;
+		}
+		else
+		{
+			GameObject.Find("Lens").GetComponent<SpriteRenderer>().enabled = true;
+		}
+	}
+}
