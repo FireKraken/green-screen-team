dc74d51d759d28c8ad7d3ff288bbb2c943bce0dd
Ashwath George <ashwath.george@gmail.com>
2014-06-18 15:11:44 -0400

    Added Project Files
    Added Unity project for the game.

---
+++ green-screen-team/Assets/Scripts/triggerTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerTest : MonoBehaviour 
+{
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("You entered the trigger");
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		Debug.Log ("You are within the trigger");
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log ("You exited the trigger");
+	}
+}
