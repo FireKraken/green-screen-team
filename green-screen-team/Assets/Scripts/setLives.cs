83854e4707642e695f3606aeb19e5691a5ae14ea
Nick Kornek <nkornek@gmail.com>
2014-06-19 16:52:32 -0400

    Menu button working

--- green-screen-team/Assets/Scripts/setLives.cs
+++ green-screen-team/Assets/Scripts/setLives.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class setLives : MonoBehaviour {
 	public static int lives = 3;
+	public Texture2D buttonImage = null;
+	public string title = string.Empty;
+	Rect position = new Rect(Screen.width/2 - Screen.width * 0.1f, Screen.height/2 - Screen.height * 0.05f, Screen.width * 0.2f, Screen.height * 0.12f);
+
+	public GUISkin skin = null;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +18,14 @@ public class setLives : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnGUI (){
+		GUI.skin = skin;
+		if (GUI.Button(position, title))
+		{
+			Application.LoadLevel(1);
+		}
+
+	}
+
 }
---
+++ green-screen-team/Assets/playVideo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playVideo : MonoBehaviour {
+	public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movTexture;
+		movTexture.loop = true;
+		movTexture.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
