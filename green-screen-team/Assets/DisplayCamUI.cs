d016c7f58ee3b4cbd67e79e930a048724a707429
Nick Kornek <nkornek@gmail.com>
2014-06-19 10:07:58 -0400

    MADNESS

---
+++ green-screen-team/Assets/DisplayCamUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayCamUI : MonoBehaviour {
+	public bool isPosed = false;
+	public GameObject Lens;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isPosed = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (isPosed = true)
+		{
+			Lens.GetComponent<SpriteRenderer>().enabled = true;
+		}
+		else
+		{
+			Lens.GetComponent<SpriteRenderer>().enabled = false;
+		}	
+	}
+}
